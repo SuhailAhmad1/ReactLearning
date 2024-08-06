4e70e84e84660d5bc1c7005087d34027c88f2af0
Suhail <suhiinu@gmail.com>
2024-08-06 22:08:36 +0530

    use-effect

---
+++ Exercise8-Side_effect/src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({ open, children }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open == true) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog className="modal" ref={dialog}>
+      {open ? children : null}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
